4ff2f7199168c710ed04fb49499367a3799ca1a2
emtmm <ernes-tutu@hotmail.com>
2012-10-18 08:54:54 -0400

    change movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,9 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+  def show
+  	id = params[:id] #retrieve movie id from uri route
+  	@movie = Movie.find(id) #lok up movie by unique ID
+  	#render app/views/movies/show.html.haml by default
+  end
 end
